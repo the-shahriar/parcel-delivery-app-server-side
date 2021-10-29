15791a1e51c4c2ea3867c913040685d5067fb754
the-shahriar <theshahriarislam@gmail.com>
2021-10-30 01:02:00 +0600

    added single data load api

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra2hb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const database = client.db("mr_mail");
+        const serviceCollection = database.collection("services");
+
+        // get all the services from database
+        app.get('/service', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // get single service
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.findOne(query);
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
