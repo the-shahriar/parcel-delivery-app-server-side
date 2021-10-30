adf0ef87902b1484f6a827cfb1e137ea9d7d020b
the-shahriar <theshahriarislam@gmail.com>
2021-10-31 00:32:01 +0600

    added delete api

--- index.js
+++ index.js
@@ -65,6 +65,22 @@ async function run() {
             });
         })
 
+        // get single booking
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
+        })
+
+        // delete a booking
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.json(result);
+        })
+
 
     } finally {
         // await client.close();
